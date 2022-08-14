2339075545213c9eb64adf11339067be06ca42fa
MID Ka <111249973+MID-Ka@users.noreply.github.com>
2022-08-15 02:46:27 +0300

    Update play.py

--- YukkiMusic/plugins/play/play.py
+++ YukkiMusic/plugins/play/play.py
@@ -19,6 +19,7 @@ import requests
 import config
 from config import BANNED_USERS, lyrical
 from strings import get_command
+from strings.filters import command
 from YukkiMusic import (Apple, Resso, SoundCloud, Spotify, Telegram,
                         YouTube, app)
 from YukkiMusic.core.call import Yukki
@@ -40,7 +41,7 @@ PLAY_COMMAND = get_command("PLAY_COMMAND")
 
 
 @app.on_message(
-    filters.command("PLAY_COMMAND")
+    command(["شغل","تشغيل"])
     & filters.group
     & ~filters.edited
     & ~BANNED_USERS
